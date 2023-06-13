899870235f7b653ce235ca50f4ddb808ce218bf1
Herbert Lim <mogulist@naver.com>
2023-06-14 00:05:38 +0900

    Shallow routing does not work for different pages

--- src/pages/first/index.tsx
+++ src/pages/first/index.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
 
 const First = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/second", undefined, { shallow: true });
+  };
+
   return (
     <main className="p-4">
       <h1>First Page</h1>
       <div>This page takes 3 seconds for server side fetch data</div>
-      <Link href="/second">Client-side navigation to Second page</Link>
+      <Link href="/second">Client-side navigation to the second page</Link>
+      <button onClick={handleClick}>Shallow route to the second page</button>
+      <div>Shallow routing only works for URL changes in the current page</div>
     </main>
   );
 };
